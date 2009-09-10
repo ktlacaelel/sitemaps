4d3aa741ba0c81d7b12d8e69a2005e61fc0020d9
kazuyoshi tlacaelel <kazu.dev@gmail.com>
2009-09-10 20:38:35 +0900

    configuration tests

--- test/configuration_test.rb
+++ test/configuration_test.rb
@@ -1,4 +1,16 @@
 require 'test_helper'
 
 class ConfigurationTest < Test::Unit::TestCase
+
+  should 'load a configuration file'
+  should 'check if configuration file exists'
+  should 'throw an error when generator is not given'
+  should 'throw an error when domain is not given'
+  should 'throw an error when targets is not given'
+  should 'throw an error when dump_dir is not given'
+  should 'retrive generator'
+  should 'retrive domain'
+  should 'retrive targets'
+  should 'retrive dump_dir'
+
 end
