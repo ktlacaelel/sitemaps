011fa2c46e49da510a50e3d05dcbfa366694697c
kazuyoshi tlacaelel <kazu.dev@gmail.com>
2009-09-10 22:09:38 +0900

    sitemaps generator tests summary

--- test/generator_test.rb
+++ test/generator_test.rb
@@ -1,4 +1,12 @@
 require 'test_helper'
 
 class GeneratorTest < Test::Unit::TestCase
+
+  should 'check for a sitemap configuration on instantiantion'
+  should 'prepare dump_dir'
+  should 'get config'
+  should 'change config'
+  should 'should download sitemaps'
+  should 'should gzip sitemaps'
+
 end
